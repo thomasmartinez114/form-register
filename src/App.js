414b0c7cb49ddb1a8da5a2595d214b1f60d1c0fe
Thomas Martinez <thomas.martinez@igt.com>
2024-02-16 21:44:52 -0500

    styled the app and form

--- src/App.js
+++ src/App.js
@@ -64,6 +64,7 @@ const App = () => {
   return (
     <div className='app'>
       <form onSubmit={handleSubmit}>
+        <h1>Register</h1>
         {inputs.map(input => (
           <FormInput
             key={input.id}
